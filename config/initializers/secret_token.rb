fb6eb9b10c4e3fca706951e18e76c58965027a48
Sergio Miguel <alu0100706468@ull.edu.es>
2014-02-18 11:24:01 +0000

    Repositorio Inicial de Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prct02::Application.config.secret_key_base = '28befb576b09fcf5154c9d678480b72be23dc3e84f3eaa81d282119b5ea7dfad3f604f0ba3119378333c5e923bb188571698951f096df12cd6ef9bbeb4ae20b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prct02::Application.config.session_store :cookie_store, key: '_prct02_session'
